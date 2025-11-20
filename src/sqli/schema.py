9c851b9908716a2d6d0e7f4561330e3f39b84542
Dwi Mulia Mokoginta <dwi-mulia-mokoginta@protonmail.com>
2025-11-20 23:02:36 +0700

    update create function to include label argument for payload insertion

--- src/sqli/schema.py
+++ src/sqli/schema.py
@@ -1,6 +1,20 @@
 from pydantic import BaseModel, Field
 
 
+class UserSearchLoginSchema(BaseModel):
+  __table__ = 'users'
+  __op__ = 'select'
+
+  username: str = Field()
+
+
+class UserEmailSearchLoginSchema(BaseModel):
+  __table__ = 'users'
+  __op__ = 'select'
+
+  email: str = Field(min_length=32, max_length=255)
+
+
 class UserLoginSchema(BaseModel):
   __table__ = 'users'
   __op__ = 'select'
