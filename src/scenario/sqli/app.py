39caf8462fdc59dd52af8a263119c6d1720e5cb3
Dwi Mulia Mokoginta <dwi-mulia-mokoginta@protonmail.com>
2025-11-29 02:50:44 +0700

    Update app.py to use aiosqlite for asynchronous database connections

--- src/scenario/sqli/app.py
+++ src/scenario/sqli/app.py
@@ -2,7 +2,9 @@ from parser import GenerateVulnSQLInjectionEndpoint
 
 from .schema import Base
 
-app = GenerateVulnSQLInjectionEndpoint(__name__, 'sqlite:///db/main.db', Base, 'sqlite')
+app = GenerateVulnSQLInjectionEndpoint(
+  __name__, 'sqlite+aiosqlite:///db/main.db', 'sqlite+aiosqlite:///db/fake.db', Base, 'sqlite'
+)
 
 
 __all__ = ['app']
